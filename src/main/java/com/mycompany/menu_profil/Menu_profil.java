d53b9e6a6ee638b01b7fb1ba2620a8ab262631cb
USER <fanto7432@gmail.com>
2024-10-07 23:13:12 +0700

    first commit

---
+++ src/main/java/com/mycompany/menu_profil/Menu_profil.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.menu_profil;
+
+/**
+ *
+ * @author USER
+ */
+public class Menu_profil {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
